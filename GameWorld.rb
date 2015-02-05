8d72bd96642467c24988e9581040f5208c7090a2
juicebbox <juicebbox@gmail.com>
2015-02-05 14:42:33 +0200

    Update GameWorld.rb
    added battle field from a string.

--- GameWorld.rb
+++ GameWorld.rb
@@ -1,10 +1,10 @@
 module GameWorld
   module BattleGround
     class Cell
-      #TO DO
-      #implement cell logic here
-      #terrain is used later to give penalty on movement of the creatures
-      #normal terrain means no penalty.
+      # TO DO
+      # implement cell logic here
+      # terrain is used later to give penalty on movement of the creatures
+      # normal terrain means no penalty.
       attr_accessor :coordinate_x, :coordinate_x, :terrain
       def initialize(coordinate_x,coordinate_y, terrain='normal')
         @coordinate_x = coordinate_x
@@ -15,19 +15,39 @@ module GameWorld
       def is_crossable?
         @terrain == 'mountain'? false : true
       end
+      
+      def terrain_type
+        @terrain
+      end
     end
 
     class Field
-      #TO DO
-      #implement field as matrix of cells
+      # TO DO
+      # implement field as matrix of cells
       attr_accessor :field
-      #the field gets cells input as string 
+      # the field gets cells input as string 
       def initialize(field)
-        @field = field
+        @field = divide_field_to_cells(field)
+      end
+      # TO DO: Fix dividing the string to cells! 
+      # TO DO: Implement method draw_map
+      def divide_field_to_cells(field)
+        new_field = []
+        field = field.lines.map(&:chomp)
+        field.each do |row|
+          row.each do |cell|
+            new_field << Cells.new(row, cell) if cell == '#'
+            new_field << Cells.new(row, cell, 'mountain') if cell == '!'
+            new_field << Cells.new(row, cell, 'water') if cell == '='
+          end
+        end
+        new_field
       end
       
-      def divide_field_to_cells(field)
-        
+      def info
+        @field.each do |cell|
+          cell.is_crossable?
+        end
       end
     end
   end
