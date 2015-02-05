9d5ddffbdea5dd56e9c2ab1834ef6c8479f57486
juicebbox <juicebbox@gmail.com>
2015-02-05 14:20:49 +0200

    Update GameWorld.rb
    Added cell coordinates and terrain type.

--- GameWorld.rb
+++ GameWorld.rb
@@ -3,13 +3,32 @@ module GameWorld
     class Cell
       #TO DO
       #implement cell logic here
-
+      #terrain is used later to give penalty on movement of the creatures
+      #normal terrain means no penalty.
+      attr_accessor :coordinate_x, :coordinate_x, :terrain
+      def initialize(coordinate_x,coordinate_y, terrain='normal')
+        @coordinate_x = coordinate_x
+        @coordinate_y = coordinate_y
+        @terrain = terrain
+      end  
+      #this method tells if the cell can be crossed by creature
+      def is_crossable?
+        @terrain == 'mountain'? false : true
+      end
     end
 
     class Field
       #TO DO
       #implement field as matrix of cells
-
+      attr_accessor :field
+      #the field gets cells input as string 
+      def initialize(field)
+        @field = field
+      end
+      
+      def divide_field_to_cells(field)
+        
+      end
     end
   end
 end
